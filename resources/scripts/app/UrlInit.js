6a6db363174ced3dfe9b9c8cdf733fbd369ab8a2
shc0743 <shc0743@outlook.com>
2023-09-22 18:39:27 +0800

    测试代码

--- resources/scripts/app/UrlInit.js
+++ resources/scripts/app/UrlInit.js
@@ -1,5 +1,6 @@
 import { RegisterUrlObject } from '../../url.js';
 
+alert('urlinit js')
 RegisterUrlObject('currentUrl', () => location.href, v => location = v);
 RegisterUrlObject('hashUrl', () => {
     const url = new URL(location.hash.substring(1), location.href);
